485556c9dc37da6741bcac63148398d4f7cf86b7
TinaJS <cristinalambardeta@gmail.com>
2024-07-17 13:04:28 +0200

    changed vercel version and vite outDir to api

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // This is the default value for Vite
+    outDir: "api", // This is the default value for Vite
   },
 });
